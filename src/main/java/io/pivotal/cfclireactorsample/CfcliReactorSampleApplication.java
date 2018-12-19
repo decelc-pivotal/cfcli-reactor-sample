8bb842aea057f42ab439ef61320170bacabd3d87
Chris Decelles <cdecelles@pivotal.io>
2018-12-19 13:14:40 -0500

    add client token provider

--- src/main/java/io/pivotal/cfclireactorsample/CfcliReactorSampleApplication.java
+++ src/main/java/io/pivotal/cfclireactorsample/CfcliReactorSampleApplication.java
@@ -6,6 +6,7 @@ import org.cloudfoundry.reactor.ConnectionContext;
 import org.cloudfoundry.reactor.DefaultConnectionContext;
 import org.cloudfoundry.reactor.TokenProvider;
 import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider;
+import org.cloudfoundry.reactor.tokenprovider.ClientCredentialsGrantTokenProvider;
 import org.cloudfoundry.reactor.uaa.ReactorUaaClient;
 import org.cloudfoundry.uaa.UaaClient;
 import org.cloudfoundry.uaa.users.ListUsersRequest;
@@ -21,11 +22,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+import javax.xml.bind.DatatypeConverter;
+import org.cloudfoundry.uaa.tokens.GetTokenByPasswordResponse;
+import org.cloudfoundry.uaa.tokens.GetTokenByPasswordRequest;
+import org.cloudfoundry.uaa.tokens.TokenFormat;
+import org.cloudfoundry.uaa.tokens.TokenKey;
+
+import org.cloudfoundry.uaa.clients.ListClientsRequest;
+import org.cloudfoundry.uaa.clients.ListClientsResponse;
+import org.cloudfoundry.uaa.clients.Client;
 @RestController
 @SpringBootApplication
 public class CfcliReactorSampleApplication {
-		
+
 	@Bean
 	DefaultConnectionContext connectionContext(@Value("${cf.apiHost}") String apiHost,
 			@Value("${cf.skipSslValidation:true}") Boolean skipSslValidation) {
@@ -35,18 +46,23 @@ public class CfcliReactorSampleApplication {
 	@Bean
 	PasswordGrantTokenProvider tokenProvider(@Value("${cf.username}") String username,
 			@Value("${cf.password}") String password) {
-		return PasswordGrantTokenProvider.builder().password(password).username(username).build();
+		return PasswordGrantTokenProvider.builder().username(username).password(password).build();
 	}
 
+	@Bean
+	ClientCredentialsGrantTokenProvider clientTokenProvider(@Value("${cf.username}") String username,
+			@Value("${cf.password}") String password) {
+		return ClientCredentialsGrantTokenProvider.builder().clientSecret(password).clientId(username).build();
+	}
 
 	@Bean
-	ReactorUaaClient uaaClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
+	ReactorUaaClient uaaClient(ConnectionContext connectionContext, ClientCredentialsGrantTokenProvider tokenProvider) {
 		return ReactorUaaClient.builder().connectionContext(connectionContext).tokenProvider(tokenProvider).build();
 	}
 
-	@Autowired 
+	@Autowired
 	UaaClient uaaClient;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CfcliReactorSampleApplication.class, args);
 	}
@@ -54,25 +70,31 @@ public class CfcliReactorSampleApplication {
 	@GetMapping("/getUserName")
 	public @ResponseBody String getUser(@RequestParam(value="id") String id) {
 		//String userId="bf9fa74d-1857-42bd-9518-6bb80410f7cf";
-		
+
 		ListUsersRequest r = ListUsersRequest.builder()
 							.filter(String.format("id eq \"%s\"", id))
 							.build();
-		
+
 		return uaaClient.users()
 				.list(r)
 				.flatMapIterable(ListUsersResponse::getResources)
 				.map(User::getUserName).blockLast();
-			
+
 	}
-	
+
 	@GetMapping("/listUsers")
-	public ResponseEntity <List<User>> listUsers() {		
+	public ResponseEntity <List<User>> listUsers() {
 		ListUsersRequest r = ListUsersRequest.builder()
 							.build();
-		
-		return uaaClient.users().list(r).map(user -> ResponseEntity.ok(user.getResources())).block();	
+
+		return uaaClient.users().list(r).map(user -> ResponseEntity.ok(user.getResources())).block();
 	}
 
-}
+	@GetMapping("/listClients")
+	public ResponseEntity <List<Client>> listClients() {
+		ListClientsRequest r = ListClientsRequest.builder()
+							.build();
 
+		return uaaClient.clients().list(r).map(client -> ResponseEntity.ok(client.getResources())).block();
+	}
+}
